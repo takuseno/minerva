7e7c9dbe70fa677c85e189bc6f6d7cd9d6c0a4d8
takuseno <takuma.seno@gmail.com>
2020-09-06 12:00:52 +0900

    Replace frame stacking with deque at breakout example

--- examples/run_breakout.py
+++ examples/run_breakout.py
@@ -3,43 +3,24 @@ import torch
 import d4rl_atari
 import gym
 
+from collections import deque
+
 # load policy function only with PyTorch dependency
 policy = torch.jit.load('policy.pt')
 
 # get the wrapped atari environment
 _, env = gym.make('breakout-mixed-v0')
 
-
-# make frame stacking helper
-class StackedObservation:
-    def __init__(self, observation_shape, n_frames):
-        self.image_channels = observation_shape[0]
-        image_size = observation_shape[1:]
-        self.n_frames = n_frames
-        stacked_shape = (self.image_channels * n_frames, *image_size)
-        self.stack = np.zeros(stacked_shape, dtype=np.uint8)
-
-    def append(self, image):
-        self.stack = np.roll(self.stack, -self.image_channels, axis=0)
-        self.stack[self.image_channels * (self.n_frames - 1):] = image.copy()
-
-    def eval(self):
-        return self.stack
-
-    def clear(self):
-        self.stack.fill(0)
-
-
-# stacking 4 frames
-stack = StackedObservation(env.observation_space.shape, 4)
-
 # evaluation loop
 while True:
+    # stacking 4 frames
+    stack = deque(np.zeros((4, 84, 84), dtype=np.uint8).tolist(), maxlen=4)
     observation = env.reset()
     while True:
-        # take action
+        # stack frame
         stack.append(observation)
-        action = policy(torch.tensor([stack.eval()], dtype=torch.uint8))[0]
+        # take action
+        action = policy(torch.tensor([list(stack)], dtype=torch.uint8))[0]
 
         # render
         env.render()
@@ -48,5 +29,4 @@ while True:
         observation, _, terminal, _ = env.step(action.cpu().numpy())
 
         if terminal:
-            stack.clear()
             break
